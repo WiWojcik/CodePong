1f54fd946468642368ceff56aaa5233841bdc435
WiWojcik <155015429+WiWojcik@users.noreply.github.com>
2024-04-17 15:08:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,149 @@
+let player1Top = 250;
+let player2Top = 250;
+const playerSpeed = 5;
+
+function p1move(keyup, keydown) {
+  if (keys[keyup] && player1Top > 0) {
+    player1Top -= playerSpeed;
+    player1.style.top = player1Top + "px";
+  }
+  if (keys[keydown] && player1Top < 550) {
+    player1Top += playerSpeed;
+    player1.style.top = player1Top + "px";
+  }
+}
+
+function p2move(keyup, keydown) {
+  if (keys[keyup] && player2Top > 0) {
+    player2Top -= playerSpeed;
+    player2.style.top = player2Top + "px";
+  }
+  if (keys[keydown] && player2Top < 550) {
+    player2Top += playerSpeed;
+    player2.style.top = player2Top + "px";
+  }
+}
+
+const keys = {};
+
+
+window.addEventListener('keydown', (event) => {
+  keys[event.key] = true;
+
+});
+
+window.addEventListener('keyup', (event) => {
+  keys[event.key] = false;
+
+});
+
+function gameLoop() {
+  p1move('w', 's');
+  p2move('ArrowUp', 'ArrowDown');
+requestAnimationFrame(gameLoop)
+
+
+}
+gameLoop();
+
+let ball = document.getElementById('ball')
+
+
+ let ballSpeedX = 2
+
+ let p1HitStraight = false
+ let p1HitUp = false
+ let p1hitDown = false
+
+ let p2HitStraight = false
+ let p2HitUp = false
+ let p2hitDown = false
+
+function ballMove(){
+  let ballLeft = parseFloat(getComputedStyle(ball).getPropertyValue('left'))
+  let ballTop = parseFloat(getComputedStyle(ball).getPropertyValue('top'))
+
+  ball.style.left = (ballLeft - ballSpeedX) + 'px'
+
+  if((ballLeft <=20 && ballLeft >= 0)){
+    let player1Top = parseFloat(getComputedStyle(player1).getPropertyValue('top'))
+    if (ballTop >= player1Top + 15 && ballTop <= player1Top + 20) { 
+      p2hitDown = false 
+      p2HitUp = false
+      p2HitStraight= false
+      p1HitStraight = true; 
+    }else if (ballTop < player1Top + 20 && ballTop > player1Top - 10 ) {
+      p2hitDown = false 
+      p2HitUp = false
+      p2HitStraight= false
+      p1HitUp = true;
+    }else if (ballTop > player1Top + 20 && ballTop < player1Top + 50) {
+      p2hitDown = false 
+      p2HitUp = false
+      p2HitStraight= false
+      p1HitUp = false
+      p1hitDown = true;   
+    }
+  }
+  if(p1HitStraight){
+    ball.style.left = (ballLeft + ballSpeedX) + 'px'
+
+  }else if(p1HitUp){
+    console.log('hitUp')
+    ball.style.left = (ballLeft + ballSpeedX) + 'px'
+    ball.style.top = (ballTop  - 0.8 ) + 'px'
+
+  }else if(p1hitDown){
+    console.log('hitDown')
+    ball.style.left = (ballLeft + ballSpeedX) + 'px'
+    ball.style.top = (ballTop + 0.8 ) + 'px'
+  }
+
+  if(ballLeft >= 570 && ballLeft <= 600){
+    let player2Top = parseFloat(getComputedStyle(player2).getPropertyValue('top'))
+    if (ballTop >= player2Top + 15 && ballTop <= player2Top + 20) { 
+      p1HitStraight = false
+      p1hitDown = false
+      p1HitUp = false
+      p2HitStraight = true
+      
+
+    }else if (ballTop < player2Top -10 + 20 && ballTop > player2Top - 10) { 
+      p1HitStraight = false
+      p1hitDown = false
+      p1HitUp = false
+      p2HitUp = true;
+    }else if (ballTop > player2Top + 20 && ballTop < player2Top + 50) { 
+      p1HitStraight = false
+      p1hitDown = false
+      p1HitUp = false
+      p2hitDown = true;   
+    }
+    
+  }
+  if(p2HitStraight){
+    console.log('xdddd')
+  }else if(p2HitUp){
+    console.log("chuj")
+    ball.style.left = (ballLeft - ballSpeedX) + 'px'
+    ball.style.top = (ballTop  - 0.8 ) + 'px'
+
+  }else if(p2hitDown){
+    console.log('p2 down')
+    ball.style.left = (ballLeft - ballSpeedX) + 'px'
+    ball.style.top = (ballTop + 0.8 ) + 'px'
+
+    
+  }
+
+
+
+    requestAnimationFrame(ballMove)
+}
+
+  
+
+
+ballMove()
+
+
